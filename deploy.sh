f89f0943848f3ddb6f41fb8269f99c3b9e4a0b46
青柠 <xyjzj277@qq.com>
2021-07-22 12:20:43 +0800

    commit

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -12,8 +12,7 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+git add .
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 const {fs, path} = require('@vuepress/shared-utils');
 
 module.exports = ctx => ({
+  base:'/xyj-vue-components-doc/',
   dest: './dist',
   locales: {
     '/': {
